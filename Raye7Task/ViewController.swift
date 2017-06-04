e969cc92e13b1cac4899957c0cb8420deb7ce7b2
Ahmed Seleem <ahmed.seleem@chestnut.com>
2017-06-05 01:18:14 +0200

    Initial Commit

---
+++ Raye7Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Raye7Task
+//
+//  Created by Ahmed Seleem on 6/5/17.
+//  Copyright © 2017 Ahmed Seleem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
